58794a30459853156c0eea41144ba379ffde01bf
Sanjana Kushwaha <sanjanakush09@gmail.com>
2026-07-25 21:31:30 +0530

    Time: 14 ms (64.04%), Space: 19.3 MB (86.74%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        s_idx = p_idx = 0
+        star_idx = s_tmp_idx = -1
+        while s_idx < len(s):
+            if p_idx < len(p) and (p[p_idx] == "?" or p[p_idx] == s[s_idx]):
+                s_idx += 1
+                p_idx += 1
+            elif p_idx < len(p) and p[p_idx] == "*":
+                star_idx = p_idx
+                s_tmp_idx = s_idx
+                p_idx += 1
+            elif star_idx == -1:
+                return False
+            else:
+                p_idx = star_idx + 1
+                s_tmp_idx += 1
+                s_idx = s_tmp_idx
+        return all(x == "*" for x in p[p_idx:])
+       
